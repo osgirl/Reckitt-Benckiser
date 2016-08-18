e0ea1878f605558c40e8873ea398d9cfaca62e40
unknown <Shiva.Bansal@ADTLPT919.adra.com>
2016-08-18 23:29:35 +0530

    Splash and Login

--- app/src/main/java/com/ad/reckittbenckiser/activities/SplashActivity.java
+++ app/src/main/java/com/ad/reckittbenckiser/activities/SplashActivity.java
@@ -1,15 +1,105 @@
 package com.ad.reckittbenckiser.activities;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.View;
+import android.view.Window;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 import com.ad.reckittbenckiser.R;
+import com.ad.reckittbenckiser.components.RadiationView;
+import com.ad.reckittbenckiser.utils.Util;
 
-public class SplashActivity extends AppCompatActivity {
+import java.util.Timer;
+import java.util.TimerTask;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+/**
+ * Created by Shiva.Bansal on 8/18/2016.
+ */
+public class SplashActivity extends Activity implements View.OnClickListener {
+
+    private RadiationView radiationView;
+
+    @Bind(R.id.frame)
+    FrameLayout frame;
+
+    @Bind(R.id.layoutKiaOra)
+    LinearLayout layoutKiaOra;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_splash);
+        ButterKnife.bind(this);
+        layoutKiaOra.setOnClickListener(this);
     }
+
+    private void startRadarAnimation() {
+        if (radiationView == null) {
+            radiationView = new RadiationView(SplashActivity.this);
+            radiationView.setBackgroundColor(Color.TRANSPARENT);
+        }
+        FrameLayout.LayoutParams radarParams = new FrameLayout.LayoutParams(
+                Util.getDeviceWidth(this), Util.getDeviceWidth(this)- Util.getDeviceWidth(this)/5);
+
+        radarParams.gravity = Gravity.CENTER_HORIZONTAL | Gravity.BOTTOM;
+        radarParams.setMargins(0, 0, 0, 0);
+        frame.removeView(radiationView);
+        frame.addView(radiationView, radarParams);
+        radiationView.startRadarAnimation();
+    }
+
+    private void stopRadarAnimation() {
+
+        if (radiationView == null)
+            return;
+        radiationView.clearAnimation();
+        radiationView.setAnimation(null);
+        frame.removeView(radiationView);
+        radiationView = null;
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        startRadarAnimation();
+        layoutKiaOra.setOnClickListener(null);
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+
+            public void run() {
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        //stopRadarAnimation();
+                        layoutKiaOra.setOnClickListener(SplashActivity.this);
+                        Intent registerIntent = new Intent(SplashActivity.this, LoginActivity.class);
+                        startActivity(registerIntent);
+
+                       /* if (mPreferenceManager.getStringData(PreferenceKeys.SESSION_TOKEN).equalsIgnoreCase("")) {
+                            Intent registerIntent = new Intent(SplashActivity.this, RegisterLoginActivity.class);
+                            startActivity(registerIntent);
+                        } else {
+                            goToHome();
+                        }*/
+                        stopRadarAnimation();
+                    }
+                });
+            }
+
+        }, 1000);
+    }
+
+
+
 }
