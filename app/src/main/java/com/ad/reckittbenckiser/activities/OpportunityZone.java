b7cc09a115d258af55cd7c3f8f1cf70eb0236594
unknown <Shiva.Bansal@ADTLPT919.adra.com>
2016-08-21 16:25:53 +0530

    Opportuniy final designs

--- app/src/main/java/com/ad/reckittbenckiser/activities/OpportunityZone.java
+++ app/src/main/java/com/ad/reckittbenckiser/activities/OpportunityZone.java
@@ -3,6 +3,8 @@ package com.ad.reckittbenckiser.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.widget.FrameLayout;
 
 import com.ad.reckittbenckiser.R;
@@ -15,7 +17,7 @@ import butterknife.OnClick;
  * Created by Shiva.Bansal on 8/20/2016.
  */
 
-public class OpportunityZone extends Activity {
+public class OpportunityZone extends AppCompatActivity {
 
     @Bind(R.id.east)
     FrameLayout eastLayout;
@@ -23,8 +25,9 @@ public class OpportunityZone extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_opportunity_zone);
+        setContentView(R.layout.activity_opporyunity_zone_design);
         ButterKnife.bind(this);
+        getSupportActionBar().setTitle("Opportunity Analysis Tool");
     }
 
     @OnClick(R.id.east)
